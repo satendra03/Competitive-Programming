0fdd029663c3a47cc4129a5512238393a246f4c9
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2024-07-31 11:40:20 +0530

    Update HalloumiBoxes.cpp

--- TLE CP/800/HalloumiBoxes.cpp
+++ TLE CP/800/HalloumiBoxes.cpp
@@ -1,1 +1,27 @@
+/*
+Code by: Satendra Kumar Parteti
+GitHub: https://github.com/satendra03
+Instagram: https://instagram.com/_satendra_03
+Approach: Any array can be sorted just by reversing its values
+*/
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        if (k == 1 and !(is_sorted(arr.begin(), arr.end()))) cout << "NO";
+        else cout << "YES";
+    }
+}
