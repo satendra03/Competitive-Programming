852114bcd0fa7fc4b189c43078268cf11e98a022
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2024-08-01 22:56:29 +0530

    Add files via upload

---
+++ TLE CP/800/UnitArray.cpp
@@ -0,0 +1,103 @@
+/*
+Code by: Satendra Kumar Parteti
+GitHub: https://github.com/satendra03
+Instagram: https://instagram.com/_satendra_03
+Approach: The condition is only possible when the count of negative is even and smaller then or equal to the count of positive
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Typedefs for convenience
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+
+// Macros for convenience
+#define FAST_IO              \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)(x).size()
+#define F first
+#define S second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i > b; --i)
+#define endl '\n'
+
+// Constants
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+// Utility functions
+template <typename T>
+void print_vector(const vector<T> &v)
+{
+    for (const auto &elem : v)
+        cout << elem << " ";
+    cout << endl;
+}
+
+template <typename T>
+void print_matrix(const vector<vector<T>> &mat)
+{
+    for (const auto &row : mat)
+    {
+        for (const auto &elem : row)
+            cout << elem << " ";
+        cout << endl;
+    }
+}
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+// Main function
+int main()
+{
+    // Fast Input and Output
+    FAST_IO;
+    // The VS Code Setup
+#ifndef ONLINE_JUDGE
+    freopen("z_input.txt", "r", stdin);
+    freopen("z_output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        unordered_map<int, int> a;
+        int x;
+        rep(i, 0, n)
+        {
+            cin >> x;
+            a[x]++;
+        }
+        if (a[1] > a[-1] and !(a[-1] % 2))
+            cout << 0 << endl;
+        else
+        {
+            int count = 0;
+            while (a[-1] > a[1])
+            {
+                count++;
+                a[-1]--;
+                a[1]++;
+            }
+            if (a[-1] % 2)
+                count++;
+            cout << count << endl;
+        }
+    }
+    return 0;
+}
